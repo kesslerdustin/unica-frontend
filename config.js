81c99426bf43f80a5493f17b58ca1162f3bdb7d5
@dustin.kessler <dustin.kessler@hs-ruhrwest.de>
2024-09-12 18:15:56 +0200

    CORS config done

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-const DEV_API_URL = 'http://localhost:3000';
+import { Platform } from 'react-native';
+
+const DEV_API_URL = Platform.OS === 'web' 
+  ? 'http://localhost:3000' 
+  : 'http://192.168.178.61:3000';
 const PROD_API_URL = 'https://unica-backend-55c11e8062f3.herokuapp.com';
 
 export const API_URL = __DEV__ ? DEV_API_URL : PROD_API_URL;
