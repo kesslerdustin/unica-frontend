9e5fa0d1d8903523ac842abf67737006e97a81d3
@dustin.kessler <dustin.kessler@hs-ruhrwest.de>
2024-09-12 17:47:31 +0200

    vercel

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const DEV_API_URL = 'http://localhost:3000';
-const PROD_API_URL = 'https://unica-backend-55c11e8062f3.herokuapp.com/';
+const PROD_API_URL = 'https://unica-backend-55c11e8062f3.herokuapp.com';
 
 export const API_URL = __DEV__ ? DEV_API_URL : PROD_API_URL;
